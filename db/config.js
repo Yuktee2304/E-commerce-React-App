428714c1a947b60cd49a843cb4036d68a4913bca
Yuktee <yuktee.@example.com>
2023-05-31 17:33:01 +0530

    Updated config.js

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const URL="mongodb://127.0.0.1:27017/e-commerce";
-
+//"mongodb://127.0.0.1:27017/e-commerce";
+const URL="mongodb+srv://Yuktee123:Yuktee123@cluster0.8nbllac.mongodb.net/e-commerce?retryWrites=true&w=majority"
 mongoose.connect(URL);
